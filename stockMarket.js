16dcdd6f45a26ad90c9b5f0aef06827f65bfaf5b
Johannes Weber <johannes@drweber.de>
2021-11-04 16:27:20 -0700

    Function implemented and more tests

--- stockMarket.js
+++ stockMarket.js
@@ -1,6 +1,24 @@
 
 const maxProfit = (prices) => {
-  
+  /*
+    Brute force: look at all permutations
+
+    for each price p
+      for each price q to p's right
+        subtract p from q and update maxDifference as needed
+    return maxDifference
+  */
+  let thisDifference = 0;
+  let maxDifference = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      thisDifference = prices[j] - prices[i];
+      if (thisDifference > maxDifference)
+        maxDifference = thisDifference;
+    }
+  }
+  return maxDifference;
+
 };
 
 module.exports = maxProfit;
--- test/stockMarket.test.js
+++ test/stockMarket.test.js
@@ -5,4 +5,16 @@ describe("maxProfit",() => {
   it("should return 16 for [45, 24, 35, 31, 40, 38, 11]", () => {
     assert.equal(maxProfit([45, 24, 35, 31, 40, 38, 11]), 16);
   });
+
+  it("should return 19 for [35, 24, 12, 11, 23, 30, 11]", () => {
+    assert.equal(maxProfit([35, 24, 12, 11, 23, 30, 11]), 19);
+  });
+
+  it("should return 14 for [10, 24]", () => {
+    assert.equal(maxProfit([10,24]), 14);
+  });
+
+  it("should return 0 for [10]", () => {
+    assert.equal(maxProfit([10]), 0);
+  });
 });
